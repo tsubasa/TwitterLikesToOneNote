eb10bbaaef1bd2f7a6a866d9962e1099ec4423d9
tsubasa <m@tsubasa.me>
2022-12-21 13:15:52 +0900

    update config

--- src/core/Config/Config.ts
+++ src/core/Config/Config.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import path from 'path';
+import _ from 'lodash';
 
 type AppConfig = {
   'twitter.clientId': string;
@@ -7,6 +8,38 @@ type AppConfig = {
   'twitter.accessToken': string;
   'twitter.refreshToken': string;
   'twitter.maxGetTweets': number;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const getIn = <T extends keyof AppConfig>(config: any, key: T): AppConfig[T] => {
+  const keyPath = key.split('.');
+  let obj = config;
+  let nest = 0;
+  while (obj && nest < keyPath.length) {
+    obj = obj[keyPath[nest]];
+    nest += 1;
+  }
+  return obj;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const hasIn = <T extends keyof AppConfig>(config: any, key: T): boolean => {
+  const keyPath = key.split('.');
+  let hasKey = true;
+  let obj = config;
+  let nest = 0;
+  while (hasKey && obj && nest < keyPath.length) {
+    hasKey = keyPath[nest] in obj;
+    obj = obj[keyPath[nest]];
+    nest += 1;
+  }
+  return hasKey;
+};
+
+const setIn = <T extends keyof AppConfig>(config: AppConfig, key: T, value: AppConfig[T]): AppConfig => {
+  const keyPath = key.split('.');
+  const newConfig = keyPath.reverse().reduce((prev, cur, idx) => ({ [cur]: idx === 0 ? value : { ...prev } }), {});
+  return _.merge(config, newConfig);
 };
 
 export default class Config {
@@ -31,7 +64,7 @@ export default class Config {
 
   private static saveConfig<T extends keyof AppConfig>(key: T, value: AppConfig[T]) {
     try {
-      this.config[key] = value;
+      this.config = setIn(this.config, key, value);
       fs.writeFileSync(this.configPath, JSON.stringify(this.config, null, 2));
     } catch (e) {
       console.error(e);
@@ -40,12 +73,12 @@ export default class Config {
 
   public static has<T extends keyof AppConfig>(key: T) {
     this.loadConfig();
-    return key in this.config;
+    return hasIn(this.config, key);
   }
 
   public static get<T extends keyof AppConfig>(key: T) {
     this.loadConfig();
-    return this.config[key];
+    return getIn(this.config, key);
   }
 
   public static set<T extends keyof AppConfig>(key: T, value: AppConfig[T]) {
